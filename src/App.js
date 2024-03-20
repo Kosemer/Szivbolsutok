0332ce2d16f0227751327bd1ef5082da9fb9aea4
Kosemer <82768146+Kosemer@users.noreply.github.com>
2024-03-20 19:19:01 +0100

    Assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Home from './Components/Pages/Home';
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer';
+import LoginForm from './Components/AdminPage/LoginForm';
 
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
