d9555a99fddaab8ec823445c0d82cf9d1adb8071
Kosemer <82768146+Kosemer@users.noreply.github.com>
2024-03-27 17:29:21 +0100

    NewNavbar

--- src/Components/Layout/NewNavbar.js
+++ src/Components/Layout/NewNavbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './NewNavbar.css'
+import React, { useState } from "react";
+import "./NewNavbar.css";
 
 const NewNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,52 +10,88 @@ const NewNavbar = () => {
 
   return (
     <div>
-      <h1>Click the burger menu to see the magic.</h1>
-      <input type="checkbox" id="burger-toggle" />
-      <label htmlFor="burger-toggle" className="burger-menu" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </label>
-      {isOpen && (
-        <div className="menu">
-          <div className="menu-inner">
-            <ul className="menu-nav">
-              <li className="menu-nav-item"><a className="menu-nav-link" href="#"><span>
-                <div>Home</div>
-              </span></a></li>
-              <li className="menu-nav-item"><a className="menu-nav-link" href="#"><span>
-                <div>About</div>
-              </span></a></li>
-              <li className="menu-nav-item"><a className="menu-nav-link" href="#"><span>
-                <div>Service</div>
-              </span></a></li>
-              <li className="menu-nav-item"><a className="menu-nav-link" href="#"><span>
-                <div>Team</div>
-              </span></a></li>
+      <div>
+        <h1>Click the burger menu to see the magic.</h1>
+        <input type="checkbox" id="burger-toggle"></input>
+        <label for="burger-toggle" class="burger-menu">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </label>
+        <div class="menu">
+          <div class="menu-inner">
+            <ul class="menu-nav">
+              <li class="menu-nav-item">
+                <a class="menu-nav-link" href="#">
+                  <span>
+                    <div>Home</div>
+                  </span>
+                </a>
+              </li>
+              <li class="menu-nav-item">
+                <a class="menu-nav-link" href="#">
+                  <span>
+                    <div>About</div>
+                  </span>
+                </a>
+              </li>
+              <li class="menu-nav-item">
+                <a class="menu-nav-link" href="#">
+                  <span>
+                    <div>Service</div>
+                  </span>
+                </a>
+              </li>
+              <li class="menu-nav-item">
+                <a class="menu-nav-link" href="#">
+                  <span>
+                    <div>Team</div>
+                  </span>
+                </a>
+              </li>
             </ul>
-            <div className="gallery">
-              <div className="title">
+            <div class="gallery">
+              <div class="title">
                 <p>Sora Gallery</p>
               </div>
-              <div className="images">
-                <a className="image-link" href="#">
-                  <div className="image" data-label="Star"><img src="https://i.loli.net/2019/11/23/cnKl1Ykd5rZCVwm.jpg" alt="" /></div>
+              <div class="images">
+                <a class="image-link" href="#">
+                  <div class="image" data-label="Star">
+                    <img
+                      src="https://i.loli.net/2019/11/23/cnKl1Ykd5rZCVwm.jpg"
+                      alt=""
+                    ></img>
+                  </div>
                 </a>
-                <a className="image-link" href="#">
-                  <div className="image" data-label="Sun"><img src="https://i.loli.net/2019/11/16/FLnzi5Kq4tkRZSm.jpg" alt="" /></div>
+                <a class="image-link" href="#">
+                  <div class="image" data-label="Sun">
+                    <img
+                      src="https://i.loli.net/2019/11/16/FLnzi5Kq4tkRZSm.jpg"
+                      alt=""
+                    ></img>
+                  </div>
                 </a>
-                <a className="image-link" href="#">
-                  <div className="image" data-label="Tree"><img src="https://i.loli.net/2019/10/18/uXF1Kx7lzELB6wf.jpg" alt="" /></div>
+                <a class="image-link" href="#">
+                  <div class="image" data-label="Tree">
+                    <img
+                      src="https://i.loli.net/2019/10/18/uXF1Kx7lzELB6wf.jpg"
+                      alt=""
+                    ></img>
+                  </div>
                 </a>
-                <a className="image-link" href="#">
-                  <div className="image" data-label="Sky"><img src="https://i.loli.net/2019/10/18/buDT4YS6zUMfHst.jpg" alt="" /></div>
+                <a class="image-link" href="#">
+                  <div class="image" data-label="Sky">
+                    <img
+                      src="https://i.loli.net/2019/10/18/buDT4YS6zUMfHst.jpg"
+                      alt=""
+                    ></img>
+                  </div>
                 </a>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
