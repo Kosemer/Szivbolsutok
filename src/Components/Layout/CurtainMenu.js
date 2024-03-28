150fc65f6b0d706818f2376686793048b5402466
Kosemer <82768146+Kosemer@users.noreply.github.com>
2024-03-28 20:22:17 +0100

    CurtainMenu

--- src/Components/Layout/CurtainMenu.js
+++ src/Components/Layout/CurtainMenu.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from 'react';
 import styles from './CurtainMenu.module.css';
 import CartContext from "../Store/cart-context";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 const CurtainMenu = () => {
 
@@ -26,10 +28,20 @@ const CurtainMenu = () => {
             &times;
           </a>
           <div className={styles.overlayContent}>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
+          <Container fluid className="mt-5">
+      <Row>
+        {cartCtx.images.map((image, index) => (
+          <Col key={index} xs={12} md={6} lg={3} className="mb-4">
+            <Card className="shadow"> {/* Árnyékolás hozzáadva */}
+              <Card.Img variant="top" src={image.src} alt={image.caption} />
+              <Card.Body className="text-center">
+                <Card.Text>{image.title}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
           </div>
         </div>
       )}
