7005c09e1b75ec2f5e3ecba411a5beae7c7cb3d5
Kosemer <82768146+Kosemer@users.noreply.github.com>
2024-04-03 20:57:04 +0200

    CategorySection

--- src/Components/PhotoGallery/CategorySection.js
+++ src/Components/PhotoGallery/CategorySection.js
@@ -3,18 +3,25 @@ import { Container } from "react-bootstrap";
 
 const SectionWithImage = ({ category, imageSrc }) => {
   return (
-    <Container fluid className="mt-5" style={{ "--bs-gutter-x": "0rem" }}>
-      <div style={{ height: "100vh", overflow: "hidden" }} className="d-flex align-items-center justify-content-center">
-        <img
-          src={imageSrc} alt={category}
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            filter: 'brightness(0.9)',
-          }}
-        />
-        <h1 className="text-center text-white position-absolute" style={{ fontSize: "4rem", fontFamily: "'Sorts Mill Goudy', serif" }}>{category}</h1>
+    <Container fluid className="mt-5" style={{ "--bs-gutter-x": "0rem", backgroundColor: "rgb(25, 25, 25)" }}>
+      <div className="d-flex">
+        {/* Kép */}
+        <div style={{ flex: 1 }}>
+          <img
+            src={imageSrc} alt={category}
+            style={{
+              width: "100%",
+              height: "100vh",
+              objectFit: "cover",
+              filter: 'brightness(0.9)',
+              borderRadius: "0 100px 100px 0px",
+            }}
+          />
+        </div>
+        {/* Szöveg */}
+        <div style={{ flex: 1, display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "rgb(25, 25, 25)" }}>
+          <h1 className="text-center text-white" style={{ fontSize: "4rem", fontFamily: "'Sorts Mill Goudy', serif" }}>{category}</h1>
+        </div>
       </div>
     </Container>
   );
