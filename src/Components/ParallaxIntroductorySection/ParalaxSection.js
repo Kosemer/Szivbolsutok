8d8b93126dff4f2c2e70ae76fc8390b12f4afa7a
Kosemer <82768146+Kosemer@users.noreply.github.com>
2024-05-25 04:49:01 +0200

    ParalaxSection

--- src/Components/ParallaxIntroductorySection/ParalaxSection.js
+++ src/Components/ParallaxIntroductorySection/ParalaxSection.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 import classes from "./ParalaxSection.module.css";
-import fruitCake from "../../Assets/CakesPicture/fruitCake.png";
+import fruitCake from "../../Assets/CakesPicture/viragLada.png";
 
 function ParalaxSection() {
   return (
     <div>
       <div className={classes.parallax}>
+        <div className={classes.titleBox}>
+      <h1 className={classes.centeredTextTitle2}>Üdvözöllek<br></br> az oldalon!</h1>
+      </div>
         <div className={classes.rectangle}>
-          <h1 className={classes.centeredTextTitle}>Üdvözöllek az oldalon!</h1>
+         
+          <div className={classes.text2}>
+          {/*<h1 className={classes.centeredTextTitle}>Üdvözöllek az oldalon!</h1>*/}
+            <p className={classes.introductoryText2}>
+              Mióta az eszemet tudom, a sütés-főzés teszi ki a gondolataim
+              jelentős részét. A sütéssel komolyabban is elkezdtem foglalkozni,
+              ezért 2017-ben elvégeztem egy cukrász képzést. Ezt követően pedig
+              megállíthatatlan süteménykészítésbe kezdtem a hagyományos és a
+              mentes vonalon is bontogatva a szárnyaimat. Mindezek eredményét
+              láthatod az oldalon.
+            </p>
+          </div>
         </div>
         <img src={fruitCake} alt="Fruit Cake" className={classes.fruitCake} />
       </div>
